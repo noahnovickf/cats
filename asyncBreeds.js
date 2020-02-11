9f67123843a649dade4e1eb1c15c4c31ec7d3b07
NoahNovick <nnovickfaille1@gmail.com>
2020-02-11 16:30:24 -0500

    tests added

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,15 +2,13 @@
 const fs = require("fs");
 
 const breedDetailsFromFile = function(breed, functionAfterData) {
-  console.log("breedDetailsFromFile: Calling readFile...");
   fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
-    if (!error) functionAfterData(data);
+    if (!error) {
+      functionAfterData(data);
+    } else {
+      functionAfterData(undefined);
+    }
   });
 };
 
-const printOutBreed = breed => {
-  console.log("Return Value: ", breed);
-};
-
-breedDetailsFromFile("Bombay", printOutBreed);
+module.exports = breedDetailsFromFile;
